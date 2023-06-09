2a11b2743f1b056e9e34395e49b15b31b4c777d5
matiassmv <66575211+matiassmv@users.noreply.github.com>
2023-06-09 15:04:39 -0400

    Update algebra.py

--- algebra.py
+++ algebra.py
@@ -1,42 +1,62 @@
 import matplotlib.pyplot as plt
 
 def pedir_coordenadas():
-    vectores = input("Ingrese vectores separados por espacio: ")
+    vectores = input("Ingrese 2 vectores separados por espacio: ")
+    vectores = vectores.replace("(", "")
+    vectores = vectores.replace(")", "")
     vectores = vectores.split(" ")
-    x = []
-    y = []
-    for elem in vectores:
-        elem = elem.split(",")
-        x.append(int(elem[0]))
-        y.append(int(elem[1]))
-    return x, y
+    u_aux = [vectores[0]]
+    v_aux = [vectores[1]]
+    u_aux = u_aux[0].split(",")
+    v_aux = v_aux[0].split(",")
+    u = []
+    v = []
+    for elem in u_aux:
+        elem = u.append(int(elem))
+    for elem in v_aux:
+        elem = v.append(int(elem))
+    return u, v
 
-def verificar_transformacion(coord_x, coord_y, T):
-    suma_x = sum(coord_x)
-    suma_y = sum(coord_y)
-    
-    for elem in coord_x:
-        suma_x = suma_x + elem
-        
+def juntar_xyz(u, v):
+    x, y, z = [], [], []
+    i = 0
+    x.append(u[0])
+    if len(u) > 1:
+        y.append(u[1])
+    if len(u) > 2:
+        z.append(u[2])
+    x.append(v[0])
+    if len(v) > 1:
+        y.append(v[1])
+    if len(v) > 2:
+        z.append(v[2])
+    return x, y, z
 
-    return 
-    
+def verificar_transformacion(u, v):
+    verf = False
+    suma_uv = []
+    for i in range(len(u)):
+        suma = u[i] + v[i]
+        suma_uv.append(suma)
+
+    return verf
+
 def mostrar_grafico(coord_x, coord_y):
     x = [0]*len(coord_x)
     y = [0]*len(coord_y)
     l = min(-1, min(coord_x)-1)
     r = max(1, max(coord_x)+1)
-    d = min(-1, min(coord_y)-1)
-    u = max(1, max(coord_y)+1)
+    b = min(-1, min(coord_y)-1)
+    t = max(1, max(coord_y)+1)
 
     plt.xlim([l, r])
-    plt.ylim([d, u])
+    plt.ylim([b, t])
     plt.axhline(0, color='black')
     plt.axvline(0, color='black')
     plt.quiver(x, y, coord_x, coord_y,  angles='xy', scale_units='xy', scale=1)
     plt.show()
 
 if __name__ == "__main__":
-    coord_x, coord_y = pedir_coordenadas()
-    transformacion = ["x","y",">","2x","2y"]
-    verificar_transformacion(coord_x, coord_y, transformacion)
+    u, v = pedir_coordenadas()
+    x, y, z = juntar_xyz(u, v)
+    verificar_transformacion(u, v)
